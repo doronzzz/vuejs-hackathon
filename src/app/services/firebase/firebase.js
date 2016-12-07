336c1d9fe19ccff364b2a84e36312abb5f1820ce
Shlomi Badusa <sbadusa@akamai.com>
2016-12-07 12:04:04 +0200

    add firebase

--- src/app/services/firebase/firebase.js
+++ src/app/services/firebase/firebase.js
@@ -13,28 +13,15 @@ const failed = () => {
 };
 
 export default () => {
-  /*
-   * Normally you would perform an AJAX-request.
-   * But to get the example working, the data is hardcoded.
-   *
-   * With the include REST-client Axios, you can do something like this:
-   * Vue.$http.get('/account')
-   *   .then((response) => {
-   *     success(response);
-   *   })
-   *   .catch((error) => {
-   *     failed(error);
-   *   });
-   */
-  const succeeds = true;
 
-  if (succeeds) {
-    success({
-      first_name: 'John',
-      last_name: 'Doe',
-      email: 'john@doe.com',
+
+
+
+  // Get a reference to the database service
+  var database = firebase.database();
+  firebase.database().ref('/stream').once('value')
+    .then(function(snapshot) {
+      console.log("once",snapshot.val());
     });
-  } else {
-    failed();
-  }
+
 };
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -175,6 +175,16 @@ require('font-awesome/less/font-awesome.less');
  */
 require('./assets/stylus/app.styl');
 
+window.config = {
+  apiKey: 'AIzaSyA-HlcV4jtbhB0sL2D74SK9RVH9oZIQgVU',
+  authDomain: 'js-fuseday-2016-stream.firebaseapp.com',
+  databaseURL: 'https://js-fuseday-2016-stream.firebaseio.com',
+  storageBucket: 'js-fuseday-2016-stream.appspot.com',
+  messagingSenderId: '533084422648',
+};
+window.firebase = require('firebase');
+
+window.firebase.initializeApp(window.config);
 
 export default {
   router,
